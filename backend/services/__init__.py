a398ddaac66f216a46236f948ddc6f07368e33ba
cgartco6 <155203611+cgartco6@users.noreply.github.com>
2025-08-15 21:33:33 +0200

    Create __init__.py

---
+++ backend/services/__init__.py
@@ -0,0 +1,4 @@
+from .payment_service import process_payment, distribute_funds
+from .ai_service import create_ai_team, execute_task, monitor_system
+from .email_service import send_email
+from .file_service import save_uploaded_file
